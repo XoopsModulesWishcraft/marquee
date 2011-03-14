1acc5702a30dd8bc79e79c448f77c70421563c3d
beckmi <beckmi@c17157d5-08f2-41e7-a131-df055f6a0da3>
2011-03-14 17:44:05 +0000

    Adding French UTF-8 Translation by Fdeconiac

--- marquee/language/french/admin.php
+++ marquee/language/french/admin.php
@@ -2,7 +2,7 @@
 /**
  * ****************************************************************************
  * marquee - MODULE FOR XOOPS
- * Copyright (c) Hervé Thouzard of Instant Zero (http://www.instant-zero.com)
+ * Copyright (c) Herv&eacute; Thouzard of Instant Zero (http://www.instant-zero.com)
  *
  * You may not change or alter any portion of this comment or credits
  * of supporting developers from this source code or any supporting source code
@@ -11,10 +11,10 @@
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
  *
- * @copyright       Hervé Thouzard of Instant Zero (http://www.instant-zero.com)
+ * @copyright       Herv&eacute; Thouzard of Instant Zero (http://www.instant-zero.com)
  * @license         http://www.fsf.org/copyleft/gpl.html GNU public license
  * @package         marquee
- * @author 			Hervé Thouzard of Instant Zero (http://www.instant-zero.com)
+ * @author 			Herv&eacute; Thouzard of Instant Zero (http://www.instant-zero.com)
  *
  * Version : $Id:
  * ****************************************************************************
@@ -25,7 +25,7 @@ define("_AM_MARQUEE_GENERALSET", "Options du module" );
 define("_AM_MARQUEE_MODULEADMIN","Administration du module");
 define("_AM_MARQUEE_DIRECTION","Direction");
 define("_AM_MARQUEE_BEHAVIOUR","Comportement");
-define("_AM_MARQUEE_STOP","Arrêter au survol de la souris");
+define("_AM_MARQUEE_STOP","ArrÃªter au survol de la souris");
 define("_AM_MARQUEE_CONTENT","Contenu");
 define("_AM_MARQUEE_EDIT","Editer");
 define("_AM_MARQUEE_DELETE","Supprimer");
@@ -33,32 +33,59 @@ define("_AM_MARQUEE_DIRECTION1","Gauche->Droite");
 define("_AM_MARQUEE_DIRECTION2","Droite->Gauche");
 define("_AM_MARQUEE_DIRECTION3","Bas->Haut");
 define("_AM_MARQUEE_DIRECTION4","Haut->Bas");
-define("_AM_MARQUEE_BEHAVIOUR1","défile");
+define("_AM_MARQUEE_BEHAVIOUR1","d&eacute;file");
 define("_AM_MARQUEE_BEHAVIOUR2","glisse");
-define("_AM_MARQUEE_BEHAVIOUR3","alterné");
+define("_AM_MARQUEE_BEHAVIOUR3","altern&eacute;");
 define("_AM_MARQUEE_ADDMARQUEE","Ajouter un marquee");
 define("_AM_MARQUEE_ACTION","Action");
-define("_AM_MARQUEE_LOOP","Répétition");
+define("_AM_MARQUEE_LOOP","R&eacute;p&eacute;tition");
 define("_AM_MARQUEE_INFINITELOOP","Infini");
 define("_AM_MARQUEE_ALIGN","Alignement");
 define("_AM_MARQUEE_ALIGN1","Haut");
 define("_AM_MARQUEE_ALIGN2","Bas");
 define("_AM_MARQUEE_ALIGN3","Milieu");
 define("_AM_MARQUEE_ADDBUTTON","Ajouter");
-define("_AM_MARQUEE_RESETBUTTON","R&eacute;initialiser");
-define("_AM_MARQUEE_ERROR_ADD_MARQUEE","Erreur, les champs n&eacute;cessaires ne sont peut &ecirc;tre pas renseign&eacute;s");
-define("_AM_MARQUEE_ADDED_OK","Le marquee a &eacute;t&eacute; ajout&eacute; avec succ&eacute;s");
-define("_AM_MARQUEE_DBUPDATED","Mise à jour de la base de donn&eacute;es avec succ&eacute;s");
-define("_AM_MARQUEE_RUSUREDEL","Etes-vous s&ucirc;r de vouloir supprimer cet &eacute;l&eacute;ment ?");
-define("_AM_MARQUEE_UPDATE","Mettre à jour");
-define("_AM_MARQUEE_BGCOLOR","Couleur de fond <br/>(pensez à mettre un #<br /> si vous spécifiez un code couleur)");
+define("_AM_MARQUEE_RESETBUTTON","R&eacute;;initialiser");
+define("_AM_MARQUEE_ERROR_ADD_MARQUEE","Erreur, les champs n&eacute;;cessaires ne sont peut &ecirc;tre pas renseign&eacute;;s");
+define("_AM_MARQUEE_ADDED_OK","Le marquee a &eacute;;t&eacute;; ajout&eacute;; avec succ&eacute;;s");
+define("_AM_MARQUEE_DBUPDATED","Mise Ã  jour de la base de donn&eacute;;es avec succ&eacute;;s");
+define("_AM_MARQUEE_RUSUREDEL","Etes-vous s&ucirc;r de vouloir supprimer cet &eacute;;l&eacute;;ment ?");
+define("_AM_MARQUEE_UPDATE","Mettre Ã  jour");
+define("_AM_MARQUEE_BGCOLOR","Couleur de fond <br/>(pensez Ã  mettre un #<br /> si vous sp&eacute;cifiez un code couleur)");
 define("_AM_MARQUEE_WIDTH","Largeur (en pixels ou en pourcentage)");
 define("_AM_MARQUEE_HEIGHT","Hauteur (en pixels)");
 define("_AM_MARQUEE_SCRAMOUNT","Nombre de pixels<br />entre deux mouvements");
 define("_AM_MARQUEE_HSPACE","Espacement horizontal (en pixels)");
 define("_AM_MARQUEE_VSPACE","Espacement vertical (en pixels)");
-define("_AM_MARQUEE_SCRDELAY","Délai entre deux mouvements<br /> en millièmes de secondes ");
-define("_AM_MARQUEE_ERROR_MODIFY_DB","Erreur pendant la mise à jour de la base de données");
-define("_AM_MARQUEE_SOURCE","source du contenu");
-define("_AM_MARQUEE_SOURCE_FIXED","Un texte fixe");
+define("_AM_MARQUEE_SCRDELAY","D&eacute;lai entre deux mouvements<br /> en milli&egrave;mes de secondes ");
+define("_AM_MARQUEE_ERROR_MODIFY_DB","Erreur pendant la mise Ã  jour de la base de donn&eacute;es");
+define("_AM_MARQUEE_SOURCE","Source du contenu");
+define("_AM_MARQUEE_SOURCE_FIXED","Texte fixe");
+
+//2.4.8
+
+// About.php
+define("_AM_MARQUEE_ABOUT_RELEASEDATE",        "Mise a jour: ");
+define("_AM_MARQUEE_ABOUT_AUTHOR",                   "Auteur: ");
+define("_AM_MARQUEE_ABOUT_CREDITS",                  "Credits: ");
+define("_AM_MARQUEE_ABOUT_LICENSE",                  "License: ");
+define("_AM_MARQUEE_ABOUT_MODULE_STATUS",            "Statut: ");
+define("_AM_MARQUEE_ABOUT_WEBSITE",                  "Site web: ");
+define("_AM_MARQUEE_ABOUT_AUTHOR_NAME",              "Nom de l\'auteur: ");
+define("_AM_MARQUEE_ABOUT_CHANGELOG",                "Change Log");
+define("_AM_MARQUEE_ABOUT_MODULE_INFO",              "Infos du module");
+define("_AM_MARQUEE_ABOUT_AUTHOR_INFO",              "Infos sur l\'auteur");
+define("_AM_MARQUEE_ABOUT_DESCRIPTION",          "Description: ");
+
+// index.php
+define("_AM_MARQUEE_ADMIN_INDEX",                  "Index");
+define("_AM_MARQUEE_ADMIN_ABOUT",                  "A propos");
+define("_AM_MARQUEE_ADMIN_HELP",                 "Aide");
+define("_AM_MARQUEE_ADMIN_SLIDES",                 "Slides");
+define("_AM_MARQUEE_ADMIN_PAGES",            "Pages");
+define("_AM_MARQUEE_ADMIN_UPDATE",                 "Mise Ã  jour");
+define("_AM_MARQUEE_ADMIN_PREFERENCES",                 "Options");
+define("_AM_MARQUEE_ADMIN_INDEX_TXT1",                 "This module lets you display on your pages with <strong>\"slideshow \"</strong> images with links to pages on your site or pages externes.<br />By default, the module is delivered with 7 flash slideshow templates, javascript or css, you can naturally incorporate or create new ones <em>(make based on those existing for example)</em> with some knowledge of development");
+
+define('_MARQUEE_ADMIN_'," "); //
 ?>
--- marquee/language/french/modinfo.php
+++ marquee/language/french/modinfo.php
@@ -2,7 +2,7 @@
 /**
  * ****************************************************************************
  * marquee - MODULE FOR XOOPS
- * Copyright (c) Hervé Thouzard of Instant Zero (http://www.instant-zero.com)
+ * Copyright (c) Herv&eacute; Thouzard of Instant Zero (http://www.instant-zero.com)
  *
  * You may not change or alter any portion of this comment or credits
  * of supporting developers from this source code or any supporting source code
@@ -11,16 +11,16 @@
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
  *
- * @copyright       Hervé Thouzard of Instant Zero (http://www.instant-zero.com)
+ * @copyright       Herv&eacute; Thouzard of Instant Zero (http://www.instant-zero.com)
  * @license         http://www.fsf.org/copyleft/gpl.html GNU public license
  * @package         marquee
- * @author 			Hervé Thouzard of Instant Zero (http://www.instant-zero.com)
+ * @author 			Herv&eacute; Thouzard of Instant Zero (http://www.instant-zero.com)
  *
  * Version : $Id:
  * ****************************************************************************
  */
-
-define("_MI_MARQUEE_DESC","Permet de créer des textes défilants");
+define("_MI_MARQUEE_NAME",		"Marquee");
+define("_MI_MARQUEE_DESC",		"Vous permet de creer des textes dynamiques (marquees)");
 define("_MI_MARQUEE_BNAME_01","Marquee 01");
 define("_MI_MARQUEE_BDESC_01","Affiche le marquee 01");
 define("_MI_MARQUEE_BNAME_02","Marquee 02");
@@ -31,18 +31,24 @@ define("_MI_MARQUEE_BNAME_04","Marquee 04");
 define("_MI_MARQUEE_BDESC_04","Affiche le marquee 04");
 define("_MI_MARQUEE_MENU_01","Gestion des marquees");
 define("_MI_MARQUEE_USE_KIOVI","Utiliser kiovi ?");
-define("_MI_MARQUEE_USE_KIOVI_DESC","S'il est détecté, kiovi sera utilisé dans l'administration");
-define("_MI_MARQUEE_METHOD","Méthode à utiliser");
-define("_MI_MARQUEE_METHOD_DESC","Si vous choisissez la méthode 'Marquee' alors le module utilisera la balise Html <i>marquee</i>, cette méthode n'est pas compatible avec tous les navigateurs mais offre plus d'options<br>Si vous choisissez la méthode <i>dhtml</i> alors la marquee devrait marcher avec plus de navigateurs mais cela nécessitera plus de code dans votre page et offrira moins d'options");
+define("_MI_MARQUEE_USE_KIOVI_DESC","S'il est d&eacute;tect&eacute;, kiovi sera utilis&eacute; dans l'administration");
+define("_MI_MARQUEE_METHOD","M&eacute;thode Ã  utiliser");
+
+
+define("_MI_MARQUEE_METHOD_DESC","Si vous selectionnez la methode 'Marquee', alors le module utilisera le tag html marquee, cette methode n'est pas compatible avec tous les navigateurs mais offre plus d'options.<br> Si vous choisissez la methode 'dhtml', alors marquee fonctionnera avec plus de navigateurs mais demandera plus de code dans vos pages et vous laissera moins d'options");
 define("_MI_MARQUEE_MARQUEE","Marquee");
 define("_MI_MARQUEE_DHTML","Javascript");
 define("_MI_MARQUEE_DATEFORMAT","Format de la date");
-define("_MI_MARQUEE_DATEFORMAT_DESC","Veuillez vous référer à la documentation de Php (<a href='http://fr.php.net/manual/fr/function.date.php' target='_blank'>http://fr.php.net/manual/fr/function.date.php</a>) pour plus d'informations. Note, si vous n'entrez rien, c'est alors le format de date par défaut qui sera utilisé");
-define("_MI_MARQUE_ITEMSCOUNT","Nombre d'éléments");
-define("_MI_MARQUE_ITEMSCOUNT_DESC","Lorsque vous choisissez d'utiliser le contenu d'un autre module (et donc pas un texte fixe) pour créer des Marquees, c'est le nombre d'éléments à utiliser");
+define("_MI_MARQUEE_DATEFORMAT_DESC","Merci de vous referrer a la documentation PHP (http://fr.php.net/manual/en/function.date.php) pour plus d\information sur le format. Note: si vous ne remplissez pas ce champs, le format de date par defaut sera utilise");
+define("_MI_MARQUE_ITEMSCOUNT","Nombre d'&eacute;l&eacute;ments");
+define("_MI_MARQUE_ITEMSCOUNT_DESC","Quand vous choisissez d'utiliser le contenu d'un autre module (et non un texte fixe) pour creer un Marquee, c'est le nombre de contenu a lister");
 define("_MI_MARQUE_TITLELENGTH","Longeur des titres");
-define("_MI_MARQUE_TITLELENGTH_DESC","Définissez la longueur des titres des éléments (0=pas de limite)");
-define("_MI_MARQUEE_TEXT_EDITOR", "Editeur de texte à utiliser");
+define("_MI_MARQUE_TITLELENGTH_DESC","D&eacute;finissez la longueur des titres des &eacute;l&eacute;ments (0=pas de limite)");
+
+
+
+
+define("_MI_MARQUEE_TEXT_EDITOR", "Editeur de texte Ã  utiliser");
 define("_MI_MARQUEE_FORM_COMPACT","Compact");
 define("_MI_MARQUEE_FORM_DHTML","DHTML");
 define("_MI_MARQUEE_FORM_SPAW","Spaw Editor");
@@ -50,4 +56,24 @@ define("_MI_MARQUEE_FORM_HTMLAREA","HtmlArea Editor");
 define("_MI_MARQUEE_FORM_FCK","FCK Editor");
 define("_MI_MARQUEE_FORM_KOIVI","Koivi Editor");
 define("_MI_MARQUEE_FORM_TINYEDITOR","TinyEditor");
+
+//2.48
+// index.php
+define("_MI_MARQUEE_ADMIN_INDEX",                  "Index");
+define("_MI_MARQUEE_ADMIN_ABOUT",                  "A propos");
+define("_MI_MARQUEE_ADMIN_HELP",                 "Aide");
+
+define('_MI_MARQUEE_MENU_00',"Accueil"); 
+define("_MARQUEE_ADMIN_HOME_DESC","Retour a l'accueil");
+
+define('_MARQUEE_ADMIN_HOME',"Marquees"); 
+define("_MARQUEE_ADMIN_MARQUEE_DESC","Gerer Marquees");
+
+define("_MARQUEE_ADMIN_ABOUT" , "A propos");
+define("_MARQUEE_ADMIN_ABOUT_DESC" , "A propos de ce module");
+
+define("_MARQUEE_ADMIN_HELP" , "Aide");
+define("_MARQUEE_ADMIN_HELP_DESC" , "Aide du module");
+
+
 ?>
