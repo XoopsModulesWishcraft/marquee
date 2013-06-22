c017c296d6db8b1df00b49f944dc1f05a4883650
cesag <cesag@c17157d5-08f2-41e7-a131-df055f6a0da3>
2013-06-22 18:57:31 +0000

    Moving kmlcsv, lexicon and marquee non english files to XoopsLanguages folder.

--- marquee/language/spanish/blocks.php
+++
@@ -1,9 +0,0 @@
-<?php
-/**
- * ****************************************************************************
- * Marquee - MODULE FOR XOOPS
- * Copyright (c) Hervé Thouzard (http://www.herve-thouzard.com)
- * ****************************************************************************
- */
-
-define("_MB_MARQUEE_SELECT","Defina qué marquesina se usará");
